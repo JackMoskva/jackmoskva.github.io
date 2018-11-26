adf1fb5ea3483bc4a1b1e6215ca0dbaf85a81216
JackMoskva <moskva84@gmail.com>
2018-11-26 19:51:56 +0200

    Add files via upload

---
+++ tor.nyc/js/main.js
@@ -0,0 +1,41 @@
+$('.sl').slick({
+	asNavFor: '.sl2'
+});
+
+$('.sl2').slick({
+	dots: false,
+	infinite: true,
+	slidesToShow: 5,
+	slidesToScroll: 1,
+	centerMode: true,
+	centerPadding: '0px',
+	arrows: true,
+	asNavFor: '.sl',
+	focusOnSelect: true,
+	responsive: [
+    {
+      breakpoint: 769,
+      settings: {
+        centerMode: true,
+        centerPadding: '0px',
+        slidesToShow: 4
+      }
+    },
+    {
+      breakpoint: 569,
+      settings: {
+        centerMode: true,
+        centerPadding: '0px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        centerMode: true,
+        centerPadding: '0px',
+        slidesToShow: 2
+      }
+    }
+  ]
+});
