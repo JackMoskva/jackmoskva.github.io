d026c5813e8eac7703472e26f68e96b211c5418a
JackMoskva <moskva84@gmail.com>
2018-12-12 05:40:29 +0200

    Add files via upload

---
+++ human/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $('.collapse.in').prev('.panel-heading').addClass('active');
+  $('#accordion, #bs-collapse')
+    .on('show.bs.collapse', function(a) {
+      $(a.target).prev('.panel-heading').addClass('active');
+    })
+    .on('hide.bs.collapse', function(a) {
+      $(a.target).prev('.panel-heading').removeClass('active');
+    });
+});
+
+$('.sl').slick({
+	arrows: false,
+	dots: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  });
