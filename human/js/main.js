ff83497b9dab5d0ab1f3af401d65733d09f83a02
JackMoskva <moskva84@gmail.com>
2018-12-14 08:36:54 +0200

    Add files via upload

--- human/js/main.js
+++ human/js/main.js
@@ -13,13 +13,33 @@ $('.sl').slick({
 	arrows: false,
 	dots: true,
     slidesToShow: 3,
-    slidesToScroll: 3
+    slidesToScroll: 3,
+    responsive: [
+    {
+      breakpoint: 769,
+      settings: {
+        centerMode: true,
+        centerPadding: '0px',
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        centerMode: true,
+        centerPadding: '0px',
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
   });
 
 $(window).scroll(function(){
     // 200px от верха
     if ($(window).scrollTop() > '200'){
-      $('.navbar').css({"background":"rgba(11, 31, 44, 0.8)"});
+      $('.navbar').css({"background":"rgba(11, 31, 44, 0.9)"});
     }
   });
 
