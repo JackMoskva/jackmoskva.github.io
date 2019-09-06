9b480f7641f13b91ed0a76f6d22e0e07ae731cec
JackMoskva <moskva84@gmail.com>
2019-09-06 13:32:36 +0300

    Add files via upload

---
+++ networking-lab/js/main.js
@@ -0,0 +1,10 @@
+var typed2 = new Typed('#typedText', {
+    strings: ['Take control of your reputation', 'Raise the next generation of leaders', 'Re-generate the outdated model'],
+    // startDelay: 1000,
+    typeSpeed: 10,
+    backSpeed: 10,
+    backDelay: 1000,
+    loop: true,
+    showCursor: false,
+    autoInsertCss: true
+  });
